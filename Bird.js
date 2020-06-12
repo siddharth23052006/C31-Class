dbd38d7ec5105022d6b3db0e5f79bd0e848c6226
siddharth23052006 <siddharthshivakumar2@gmail.com>
2020-06-12 20:01:30 +0530

    Made smoke effect

--- Bird.js
+++ Bird.js
@@ -2,11 +2,21 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.trajectory = [];
+    this.smoke = loadImage("sprites/smoke.png");
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+    if (this.body.velocity.x > 6 && this.body.position.x >200){
+      var pos = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(pos);
+    }
+    for (var i = 0; i<this.trajectory.length; i++){
+      //imageMode(CENTER);
+      image(this.smoke,this.trajectory[i][0],this.trajectory[i][1]);
+    }
 
     super.display();
   }
